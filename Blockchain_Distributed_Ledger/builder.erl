3713da44ad377a2f510ed84d76d5e7fc1c1e1225
romain-mottet <romain.mottet@student.uclouvain.be>
2023-12-05 15:27:29 +0100

    add

--- Blockchain_Distributed_Ledger/builder.erl
+++ Blockchain_Distributed_Ledger/builder.erl
@@ -2,6 +2,7 @@
 % Implement a broadcast step to disseminate a block to all other nodes.
 -module(builder).
 -export([start/1, create_block/1, broadcast_block/2]).
+-include("csv_reader.erl").
 
 % Function to start a new builder with a given address
 start(Address) ->
@@ -23,36 +24,21 @@ read_transactions(FilePath) ->
 create_block(Address, Transactions) ->
     Block = #block{
         block_number = get_next_block_number(),
-        merkle_tree_root = calculate_merkle_tree_root(Transactions),
         builder_address = Address,
-        last_block_hash = get_last_block_hash(),
         transactions = Transactions
     },
-    broadcast_block(Address, Block).
 
-% Function to broadcast a block to all nodes
-broadcast_block(Address, Block) ->
-    Nodes = get_all_nodes(),
-    lists:foreach(fun(Node) -> node:sends_messages(Node, {block, Block}) end, Nodes).
+
 
 % Placeholder functions for future implementation
 get_next_block_number() ->
     % Implement logic to get the next block number
 
-
-calculate_merkle_tree_root(Transactions) ->
-    % Implement Merkle tree calculation logic
-
-
-get_last_block_hash() ->
-    % Implement logic to get the hash of the last published block
     
 
 
 -record(block, {
     block_number,
-    merkle_tree_root,
     builder_address,
-    last_block_hash,
     transactions
 }).
