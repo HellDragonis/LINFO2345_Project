9264f506583ac0ced0b6101da80da86691f03f83
romain-mottet <romain.mottet@student.uclouvain.be>
2023-12-05 10:11:28 +0100

    Ajout creation node

--- Blockchain_Distributed_Ledger/node.erl
+++ Blockchain_Distributed_Ledger/node.erl
@@ -1,21 +1,19 @@
-% Implement a node that sends and receives messages.
-
 -module(node).
 -export([start/1, sends_messages/2]).
 
 % Function to start a new node with a given address and register it
 start(Address) ->
-    Pid = spawn(fun() -> node_loop(Address) end),
-    register(Address, Pid),
+    AtomAddress = list_to_atom(Address),
+    Pid = spawn(fun() -> node_loop(AtomAddress) end),
+    register(AtomAddress, Pid),
     Pid.
-
 
 % Node main loop, it implements the "receives messages" asked
 node_loop(Address) ->
     receive
         % Handling incoming messages
         {From, Message} ->
-            handle_message(From, Message),
+            handle_message(Address, From, Message),
             node_loop(Address);
         % Stop signal
         stop ->
@@ -24,9 +22,9 @@ node_loop(Address) ->
 
 
 % Handling incoming messages, to make the transaction
-handle_message(From, Message) ->
+handle_message(Address, From, Message) ->
     % Implement the transaction asked here, and send an acknowledgement (TODO)
-    io:format("Node ~s received message from ~s: ~p~n", [Address, From, Message]).
+    io:format("Node ~s received message from ~p: ~p~n", [Address, From, Message]).
 
 
 % Function to send a message to another node
@@ -36,5 +34,5 @@ sends_messages(To, Message) ->
     receive
         {To, _} -> ok
     after 5000 ->
-        io:format("Timeout sending message to ~s~n", [To])
+        io:format("Timeout sending message to ~p~n", [To])
     end.
